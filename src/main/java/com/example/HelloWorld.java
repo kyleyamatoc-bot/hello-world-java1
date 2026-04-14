e05a2175b3e058da9f66b31ba796877ad4ff6a12
kylayamatoc <kyleyamatoc@gmail.com>
2026-04-14 20:43:03 +0800

    Update greeting message in HelloWorld class to include 'TEST'

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -2,10 +2,10 @@ package com.example;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); //comment
+        System.out.println("Hello, World!, TEST"); //comment
     }
 
     public String getGreeting() {
-        return "Hello, World!";
+        return "Hello, World!, TEST";
     }
 }
