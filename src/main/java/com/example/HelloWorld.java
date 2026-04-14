224ec97287e515de037e23543d19efa4e8fcee9e
kylayamatoc <kyleyamatoc@gmail.com>
2026-04-14 20:32:38 +0800

    git commit -m "Trigger Jenkins pipeline"

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -2,10 +2,10 @@ package com.example;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); //comment
+        System.out.println("Hello, and welcome to the World!"); //comment
     }
 
     public String getGreeting() {
-        return "Hello, World!";
+        return "Hello, and welcome to the World!";
     }
 }
